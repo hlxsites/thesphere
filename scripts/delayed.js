3a8033299d8ebbc6885e4225b8dad5c5f76083f3
Alexandre Capt <acapt@adobe.com>
2022-09-01 11:12:49 +0200

    feat: GTM

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,22 @@ import { sampleRUM } from './scripts.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+window.fsData = [{
+  page: {
+    pageInfo: {
+      pageName: 'Sphere website',
+      geoRegion: 'global',
+      language: 'en',
+    },
+    category: {
+      primaryCategory: 'investors',
+      subCategory1: 'events',
+    },
+  },
+}];
+
+// Google Tag Manager
+// eslint-disable-next-line
+(function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, 'script', 'dataLayer', 'GTM-KL3PT7C');
+// End Google Tag Manager
