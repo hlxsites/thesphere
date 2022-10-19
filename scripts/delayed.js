440de9b078f49fa0196d67923dd81b017feac77b
Abhishek <abhishek.yadav@f-secure.com>
2022-10-19 10:10:04 +0300

    Change in GTM Container

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -49,7 +49,7 @@ gtag('set', 'ads_data_redaction', true);
 
 // Google Tag Manager
 // eslint-disable-next-line
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KL3PT7C');
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PQFSS83');
 
 // OneTrust Cookies Consent Notice start
 
