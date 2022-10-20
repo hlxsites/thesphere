ff832fe7c717b89844fc200882d1e9981f2d84de
Abhishek <abhishek.yadav@f-secure.com>
2022-10-20 11:14:25 +0300

    Adding new script

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -20,36 +20,44 @@ sampleRUM('cwv');
 
 // add more delayed functionality here
 
-// Global site tag (gtag.js) - Google Analytics
+// Global site tag (gtag.js) - Google Analytics Start
 
-// Initialize the data layer for Google Tag Manager
-// (this should mandatorily be done before the Cookie Solution is loaded)
+// Initialize the data layer for Google Tag Manager (this should mandatorily be done before the Cookie Solution is loaded)
 window.dataLayer = window.dataLayer || [];
-
-function gtag(...args) {
-  window.dataLayer.push(args);
+function gtag() {
+  dataLayer.push(arguments);
 }
-
-// Default consent mode is "denied" for both ads and analytics as well as the optional types,
-// but delay for 2 seconds until the Cookie Solution is loaded
-gtag('consent', 'default', {
-  ad_storage: 'denied',
-  analytics_storage: 'denied',
-  functionality_storage: 'denied', // optional
-  personalization_storage: 'denied', // optional
-  security_storage: 'denied', // optional
-  wait_for_update: 2000, // milliseconds
+// Default consent mode is "denied" for both ads and analytics as well as the optional types, but delay for 2 seconds until the Cookie Solution is loaded
+gtag("consent", "default", {
+  ad_storage: "denied",
+  analytics_storage: "denied",
+  functionality_storage: "denied", // optional
+  personalization_storage: "denied", // optional
+  security_storage: "denied", // optional
+  wait_for_update: 2000 // milliseconds
 });
 
 // Improve ad click measurement quality (optional)
 gtag('set', 'url_passthrough', true);
 
 // Further redact your ads data (optional)
-gtag('set', 'ads_data_redaction', true);
+gtag("set", "ads_data_redaction", true);
 
-// Google Tag Manager
-// eslint-disable-next-line
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PQFSS83');
+//Global site tag (gtag.js) - Google Analytics End-->
+
+//Google Tag Manager Start
+
+(function (w, d, s, l, i) {
+  w[l] = w[l] || []; w[l].push({
+    'gtm.start':
+      new Date().getTime(), event: 'gtm.js'
+  });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-PQFSS83');
+
+//End Google Tag Manager
 
 // OneTrust Cookies Consent Notice start
 
@@ -59,5 +67,5 @@ loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', {
   'data-domain-script': 'f7c01035-72d2-4100-8be0-2b69fc01a195',
 });
 
-window.OptanonWrapper = () => {};
+window.OptanonWrapper = () => { };
 // OneTrust Cookies Consent Notice end
